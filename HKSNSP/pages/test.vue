e4d4fddf324cde6efa06efacfa3a3a3b6f100950
yuudati4 <k22126kk@aitech.ac.jp>
2023-03-04 21:19:10 +0900

    今度こそは

---
+++ HKSNSP/pages/test.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>
+    <v-img
+      :width="900"
+      aspect-ratio="1/1"
+      cover
+      src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+    ></v-img>
+    <v-checkbox label="test"></v-checkbox>
+  </h1>
+</template>
